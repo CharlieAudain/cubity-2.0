a142a24023128a3cff0cd6da82286771fcb1c1ba
CharlieAudain <audain27@gmail.com>
2025-10-20 16:03:17 +0100

    assigned scramble propTypes and defaultProps

--- src/components/Scramble.js
+++ src/components/Scramble.js
@@ -1,10 +1,19 @@
 import React, { Component } from "react";
-
+import PropTypes from "prop-types";
 class Scramble extends Component {
+  static propTypes = {
+    scramble: PropTypes.string.isRequired,
+  };
+
+  static defaultProps = {
+    scramble: "R2 B R B2 R2 L D2 R2 L R L2 B U2 R2 U2 R2 B2 U F D",
+  };
   render() {
+    const { scramble } = this.props;
+
     return (
       <div>
-        <h4>{this.props.scramble}</h4>
+        <h4>{scramble}</h4>
       </div>
     );
   }
