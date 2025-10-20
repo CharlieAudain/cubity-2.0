5a4a56dcb90d4a2e82c9222d12890e3eaf1441fd
CharlieAudain <audain27@gmail.com>
2025-10-20 16:05:25 +0100

    Assigned tips proptypes and default props

--- src/components/Tips.js
+++ src/components/Tips.js
@@ -1,8 +1,16 @@
 import React, { Component } from "react";
+import PropTypes from "prop-types";
 
 class Tips extends Component {
+  static propTypes = {
+    tip: PropTypes.string.isRequired,
+  };
+
+  static defaultProps = {
+    tip: "Use Spacebar to control the timer!"
+  }
   render() {
-    const {tip} = this.props
+    const { tip } = this.props;
     return (
       <div>
         <h4>{tip}</h4>
