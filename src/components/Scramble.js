ef548d15ad2a5cd5fc7144cd33df1e8dc3f68cfa
CharlieAudain <audain27@gmail.com>
2025-10-20 18:45:07 +0100

    initial timer state commit

--- src/components/Scramble.js
+++ src/components/Scramble.js
@@ -1,5 +1,7 @@
 import React, { Component } from "react";
 import PropTypes from "prop-types";
+
+
 class Scramble extends Component {
   static propTypes = {
     scramble: PropTypes.string.isRequired,
--- src/components/Timer.js
+++ src/components/Timer.js
@@ -3,9 +3,11 @@ import Scramble from "./Scramble";
 import Controls from "./Controls";
 
 class Timer extends Component {
+  state = { ready: false, running: false };
+
   render() {
     return (
-      <div className = "container">
+      <div className="container">
         <Scramble />
         <h1 className="timer">0.00</h1>
         <Controls />
